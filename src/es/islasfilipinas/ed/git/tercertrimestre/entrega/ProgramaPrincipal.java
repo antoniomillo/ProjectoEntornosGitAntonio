8c960e60ee9cef4618eaa55c6011fdb2da7791f3
antoniomillo <83137575+antoniomillo@users.noreply.github.com>
2021-04-25 21:13:13 +0200

    Versión 1.0 Primer guardado del projecto, que almacena los datos de usuarios Versión no eficaz (bad smells)

---
+++ src/es/islasfilipinas/ed/git/tercertrimestre/entrega/ProgramaPrincipal.java
@@ -0,0 +1,60 @@
+package es.islasfilipinas.ed.git.tercertrimestre.entrega;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Voy a crear una estructura de pares de clave valor.
+ * Esta estructura lo que va a hacer es guardar 
+ * la información de un usuario:
+ * - Id
+ * - NombreUsuario 
+ * - Contraseña
+ * - Apellido1
+ * - Apellido2
+ * - Email
+ * 
+ * Como clave aparecerán lo anteriormente puesto y como
+ * valor a pareceran los datos del usuario.
+ * 
+ * @author millo
+ *
+ */
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+
+		//Creo la misma estructura HashMap para cada 
+		//nueva entrada de datos de otro usuario
+		Map<String, String> usuario1 = new HashMap<>();
+		usuario1.put("Id", "001");
+		usuario1.put("Nombre Usuario", "usuario1");
+		usuario1.put("Contraseña","xxxxxxxxxxxxxxxxxx");
+		usuario1.put("Nombre", "Pepe");
+		usuario1.put("Primer-Apellido","Gonzalez");
+		usuario1.put("Segundo-Apellido","Garcia");
+		usuario1.put("Email", "pepegonzogar@gmail.com");
+		
+		Map<String, String> usuario2 = new HashMap<>();
+		usuario2.put("Id", "002");
+		usuario2.put("Nombre Usuario", "usuario2");
+		usuario2.put("Contraseña","xxxxxxxxxxxxxxxxxx");
+		usuario2.put("Nombre", "Mónica");
+		usuario2.put("Primer-Apellido","Castel");
+		usuario2.put("Segundo-Apellido","Rios");
+		usuario2.put("Email", "moncasrio@gmail.com");
+		
+		Set dame = usuario1.keySet();
+		Iterator <String> it = dame.iterator();
+		String key;
+		String tmp;
+		while (it.hasNext()) {
+			key = it.next();
+			tmp = usuario1.get(key);
+			System.out.println(key+": "+tmp);
+		}
+	}
+
+}
